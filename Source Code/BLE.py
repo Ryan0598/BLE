aa90f0fb3297d2f5258f27bed3144509f2725aed
Ryan0598 <44226763+Ryan0598@users.noreply.github.com>
2020-02-15 17:47:42 +0000

    V2.0
    updates,  removed unused code

--- Source Code/BLE.py
+++ Source Code/BLE.py
@@ -3,14 +3,8 @@
 #	AUTHOR : RYAN PARKINS
 #
 
-# https://github.com/karulis/pybluez - pybluez library
-# sudo apt-get install libbluetooth-dev
-# sudo apt-get install pkg-config
-# sudo apt-get install libboost-python-dev
-# sudo apt-get install libboost-thread-dev
-# sudo apt-get install libbluetooth-dev
-# sudo apt-get install libglib2.0-dev
-# sudo apt-get install python-dev
+
+
 # sudo pip install gattlib
 # sudo pip install matplotlib
 # sudo apt install python-tk
@@ -18,16 +12,25 @@
 # sudo pip install ptable
 # sudo pip install art
 # sudo pip install pandas
+#
+#
+#
+# sudo apt-get install libbluetooth-dev
+# sudo apt-get install pkg-config
+# sudo apt-get install libboost-python-dev
+# sudo apt-get install libboost-thread-dev
+# sudo apt-get install libbluetooth-dev
+# sudo apt-get install libglib2.0-dev
+# sudo apt-get install python-dev
+
 
 
 # relevant imports
 import datetime
-
 import random
 import os
 import tkMessageBox
 from datetime import datetime
-
 from gattlib import DiscoveryService
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -68,12 +71,16 @@ time = 0
 scanCount = time * scanCount
 scanTime = 5
 scanRef = ''
-os.system('cls||clear')
-print(art)
+os.system('cls||clear')#clear screen
 
-print('\n Welcome to BLE Scanner, Enter a scan reference below in order to start the scan. \n Data will be diplayed here in a graphical format, simply press the "save" button on the graph in order to save a copy (Image). \n Mac Data will be saved in the form of a text file and will be named with relation to the scan reference.')
+
+print(art)#print ble scanner logo
+
+#print welcome message
+print('\n Welcome to BLE Scanner, Enter a scan reference below in order to start the scan. \n Data will be diplayed here in a graphical format, simply press the "save" button on the graph in order to save a copy (Image). \n Mac Data will be saved in the form of a text file and will be named with relation to the scan reference. \n\n in order to identify specific devices, simply create or edit a text file in the code directory, \n Simply write into the file, (Each Line) MAC_Address Reference')
 scanRef = raw_input("\n Scan Reference : ")
 
+#create a dictionary from the blacklist text file, gathering addresses and descriptions of devices being searched for
 blackListDict = {}
 with open ("BLACKLIST.txt") as f:
 	for line in f:
@@ -92,24 +99,18 @@ s.write ("Bluetooth Low Energy Scan, Reference : " + str(scanRef) + " - Date And
 #########################################################################
 
 
+#set a matplotlib figure with 2 subplots
 fig, (ax1,ax2) = plt.subplots(2, sharey=True)
+
+#set some parameters for the chart, font and titles
 plt.rcParams.update({'font.size': 22})
-
 fig.suptitle('Bluetooth Low Energy Scan', fontsize=20, fontweight='bold')
-
-
 plt.style.use('fivethirtyeight')
 plt.title('BLE Scan', fontsize=18, fontweight='bold')
-
-
-
 ax1.yaxis.set_major_locator(MaxNLocator(integer=True))
 ax1.set(title = 'Bluetooth Low Energy - Device Identification', ylabel = 'Device Count')
-
-
 ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
 ax2.set(xlabel = 'Time (Seconds)', ylabel = 'Device ID')
-
 
 
 #########################################################################
@@ -132,11 +133,15 @@ def lescan():
 	global time_list
 
 
+# set the service to a discoveryservice from the bluetooth adapter, HCI0 for internal adapter, HCI1 for external
 	service = DiscoveryService("hci0")
+	#carry out a LE scan for the time specified
 	ScanDevices = service.discover(scanTime)
+	#set variables to 0 to clear from previous scan
 	count = 0
 	knownCount = 0
 	unknownCount = 0
+	#increase time by the scan time, in order to increase the time_passed
 	time = time + scanTime
 	time_passed.append(time)	
 	
@@ -148,6 +153,8 @@ def lescan():
 	s.write("\n -------------------------------------------")
 	s.write("\n\n Scan Time : " + str(time))
 	
+	
+	#for each device found in the scan, add 1 to the amount of devices found, and add these to the graph lists
 	for address, name in ScanDevices.items():
 		count = count + 1
 		addresses.append(format(address))
